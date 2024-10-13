87a8b7992d5a9307e6fec5af9c11e56398f6fcb6
equilibrium139 <zaidruwaishan@gmail.com>
2024-10-13 11:16:00 -0400

    create signup endpoint

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const multer = require("multer");
 const fs = require("fs");
 const cors = require("cors");
 const app = express();
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
 const port = 8080;
 const pg = require("pg");
 const Pool = pg.Pool;
@@ -12,6 +14,9 @@ pool.on("error", (err, client) => {
     console.error("Unexpected error on idle client", err);
     process.exit(-1);
 });
+
+const uniqueViolationCode = "23505";
+const sessionIDKey = "awsid";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -32,8 +37,12 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -95,119 +104,32 @@ app.post("/api/assets/upload", upload.fields([{ name: 'asset', maxCount: 1 }, { 
     }
 });
 
-app.use(express.static("public"));
+app.post("/signup", async (req, res) => {
+    try {
+        const password_hash = await bcrypt.hash(req.body.password, 10);
+        const query = {
+            text: `INSERT INTO 
+                    users(username, email, password_hash, created_at, updated_at)
+                    VALUES($1, $2, $3, clock_timestamp(), clock_timestamp())`,
+            values: [req.body.username, req.body.email, password_hash]
+        };
+        const queryResult = await pool.query(query);
+        const newUser = queryResult.rows[0];
+        res.status(200).cookie(sessionIDKey, crypto.randomUUID(), { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true, sameSite: "none" })
+            .json({ id: newUser.id, username: newUser.username, email: newUser.email });
+    } catch (error) {
+        console.error("Error registering user: ", error);
+        let errorMessage = "Unknown error";
+        if (error.code === uniqueViolationCode) {
+            if (error.constraint === "unique_username") {
+                errorMessage = "Username already exists";
+            }
+            else if (error.constraint === "unique_email") {
+                errorMessage = "Email already exists";
+            }
+        }
+        res.status(400).json({ error: errorMessage });
+    }
+});
 
-const dummyAssets = [
-    {
-        id: 1,
-        name: "teapot",
-        description: "A wonderful teapot",
-        file_url: "teapot.obj",
-        thumbnail_url: "teapot.jpg",
-        created_by: 1,
-        created_at: "2024-10-07T20:29:07.474437-04:00",
-        updated_at: "2024-10-07T20:29:07.474437-04:00",
-        tags: ["3D", "amazing", "teapot"],
-        is_public: true,
-        downloads: 0,
-        views: 1,
-    },
-    {
-        id: 2,
-        name: "bunny",
-        description: "A beautiful bunny",
-        file_url: "bunny.obj",
-        thumbnail_url: "bunny.jpg",
-        created_by: 1,
-        created_at: "2024-10-07T20:40:28.642942-04:00",
-        updated_at: "2024-10-07T20:40:28.642942-04:00",
-        tags: ["furry", "cute"],
-        is_public: true,
-        downloads: 0,
-        views: 2,
-    },
-    {
-        id: 3,
-        name: "dragon",
-        description: "A dangerous dragon",
-        file_url: "dragon.obj",
-        thumbnail_url: "dragon.jpg",
-        created_by: 1,
-        created_at: "2024-10-07T20:41:42.450059-04:00",
-        updated_at: "2024-10-07T20:41:42.450059-04:00",
-        tags: ["scaly", "ferocious", "big", "scary"],
-        is_public: true,
-        downloads: 0,
-        views: 3,
-    },
-    {
-        id: 4,
-        name: "monkey",
-        description: "A goofy monkey named Suzanne",
-        file_url: "monkey.obj",
-        thumbnail_url: "monkey.jpg",
-        created_by: 1,
-        created_at: "2024-10-07T20:42:45.852391-04:00",
-        updated_at: "2024-10-07T20:42:45.852391-04:00",
-        tags: ["goofy", "blender", "big eyes"],
-        is_public: true,
-        downloads: 0,
-        views: 4,
-    },
-    {
-        id: 5,
-        name: "buddha",
-        description: "Some zen guy",
-        file_url: "buddha.obj",
-        thumbnail_url: "buddha.jpg",
-        created_by: 1,
-        created_at: "2024-10-07T20:45:31.257504-04:00",
-        updated_at: "2024-10-07T20:45:31.257504-04:00",
-        tags: ["zen", "buddhist", "fatty"],
-        is_public: true,
-        downloads: 0,
-        views: 0,
-    },
-    {
-        id: 6,
-        name: "duck",
-        description: "Quack",
-        file_url: "duck.obj",
-        thumbnail_url: "duck.jpg",
-        created_by: 2,
-        created_at: "2024-10-07T20:48:26.057556-04:00",
-        updated_at: "2024-10-07T20:48:26.057556-04:00",
-        tags: ["quack", "yellow"],
-        is_public: true,
-        downloads: 0,
-        views: 0,
-    },
-    {
-        id: 7,
-        name: "cube",
-        description: "Just a boring old cube",
-        file_url: "cube.obj",
-        thumbnail_url: "cube.jpg",
-        created_by: 2,
-        created_at: "2024-10-07T20:49:44.789352-04:00",
-        updated_at: "2024-10-07T20:49:44.789352-04:00",
-        tags: ["6 faces", "8 vertices"],
-        is_public: true,
-        downloads: 0,
-        views: 0,
-    },
-    {
-        id: 8,
-        name: "cesium man",
-        description: "A curious character",
-        file_url: "cesium-man.obj",
-        thumbnail_url: "cesium-man.jpg",
-        created_by: 2,
-        created_at: "2024-10-07T20:50:42.283171-04:00",
-        updated_at: "2024-10-07T20:50:42.283171-04:00",
-        tags: ["animated", "textured"],
-        is_public: true,
-        downloads: 0,
-        views: 0,
-    },
-];
+app.use(express.static("public"));
